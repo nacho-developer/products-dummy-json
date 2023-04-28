7c4629651d8a5dd3b29613a9f3bc0b65c784c6ca
Nacho <nacho.dominguez.dev@gmail.com>
2023-04-28 12:47:13 +0200

    modify cache

--- src/main/java/com/opendit/prueba/config/CacheConfig.java
+++ src/main/java/com/opendit/prueba/config/CacheConfig.java
@@ -2,32 +2,27 @@ package com.opendit.prueba.config;
 
 import java.util.List;
 
-import org.springframework.cache.Cache;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Configuration
 @EnableCaching
+@Slf4j
 public class CacheConfig {
 
 	@Bean
 	CacheManager cacheManager() {
+		log.info("Init cache manager");
 	    ConcurrentMapCacheManager cacheManager = new ConcurrentMapCacheManager();
 	    cacheManager.setCacheNames(List.of("allProducts", "highestPricedProductCache", "lowestPricedProductCache",
 	            "averagePricedProductCache", "productsByBrandCache", "productsByCategoryCache", "allUsers",
-	            "cartByUserId"));
+	            "findByUserId"));
 	    
-
-	    // Set TTL for all caches to 10 minutes
-//	    for (String cacheName : cacheManager.getCacheNames()) {
-//	        Cache cache = cacheManager.getCache(cacheName);
-//	        Object cacheValue = cache.get(cacheName).get(); // obtain the actual value from the ValueWrapper
-//	        cache.put(cacheName, cacheValue); // use cacheValue as the value argument
-//	    }
-        
 	    return cacheManager;
 	}
 
